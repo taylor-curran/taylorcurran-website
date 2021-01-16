481404f8d7919a74147d92d15c01bf1efcfa03ae
taylor-curran <taylorcurranagtc@gmail.com>
2021-01-15 19:02:48 -0500

    Populating First Models

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# Each Row is an Object of the Class  
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=30)
+    image = models.FilePathField(path="/projects/img")
